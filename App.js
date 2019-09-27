fae02126b7c7cc7021d65c2012a49b49dc723a8a
KevinJS <iammaindiana@gmail.com>
2019-09-27 07:46:15 -0500

    Complete Login

--- App.js
+++ App.js
@@ -1,32 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import firebase from 'firebase';
 
-//IMPORTS EXTERNOS
-import Login from './/componets/login';
-import Home from './/componets/home';
-import Register from './/componets/register';
-
-
-const AppNavigator = createStackNavigator({
-  Login : {
-    screen:Login
-  },
-  Register :{
-    screen:Register
-  },
-  Home:{
-    screen: Home
-  },
-  
-},{initialRouteName:'Login'});
-
-
-
-export default createAppContainer(AppNavigator);
-=======
 import React, { Component } from "react";
 import {createAppContainer,createSwitchNavigator} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack'
@@ -57,4 +29,4 @@ export default createAppContainer(
     }
   )
 );
->>>>>>> 17bcfab61e1106881e15a4bf09ea9266ebe5ec89
+
