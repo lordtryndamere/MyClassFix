c1be41c247a3830bb67408f6a1511f02b40cd941
KevinJS <iammaindiana@gmail.com>
2019-11-07 17:03:31 -0500

    Haciendo el componioente}

--- components/MyCalendar/ReservarClase.js
+++ components/MyCalendar/ReservarClase.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import 'moment/locale/es'
 import Toast from 'react-native-simple-toast';
-import { View, Text, StyleSheet, ScrollView,TouchableOpacity,Alert } from 'react-native'
+import { View, Text, StyleSheet, ScrollView,TouchableOpacity,Alert ,Image,FlatList} from 'react-native'
 import * as firebase from 'firebase'
-import { Header, Icon, ListItem } from 'react-native-elements'
-import {  Calendar } from 'react-native-calendars'
+import { Header, Icon, ListItem,Card } from 'react-native-elements'
+// import {  Calendar } from 'react-native-calendars'
 import moment from 'moment'
 import {
     widthPercentageToDP as ancho,
@@ -14,13 +15,14 @@ import {
 
 
 
-
+var Data  = []
 export default class ReservasClase extends Component {
     constructor(props) {
         super(props)
         this.state = {
             
             ...props,
+            isLoading:"",
             calendar: [],
             key: this.props.navigation.state.params.key,
             skill:this.props.navigation.state.params.sk,
@@ -30,7 +32,8 @@ export default class ReservasClase extends Component {
             newSkill:this.props.navigation.state.params.newSkill,
             day:null,
             keycalendar:"",
-            time:""
+            time:"",
+            EndTime:""
 
         }
 
@@ -66,23 +69,18 @@ export default class ReservasClase extends Component {
     }
 
  
-    loadCalendar = (day) => {
+    loadCalendar = () => {
 
-        this.setState({day:day})
     
         const { key } = this.state
         var Dates = []
-        var startTime = parseInt(moment(day.dateString).format('x'))
-        var endTime = parseInt(moment(day.dateString).add(1, 'days').format('x'))
-         this.setState({time:day.dateString})   
+        var startTime = parseInt(moment().format('x'))
         
         
 
         
-
-        if(  moment(day.dateString).format('YYYY-MM-DD') >=  moment().format('YYYY-MM-DD') )
-        {   
-            firebase.database().ref(`/teachers/${key}/newCalendar/week`).orderByChild('date').startAt(startTime).endAt(endTime).on('value', snapshot => {
+   
+            firebase.database().ref(`/teachers/${key}/newCalendar/week`).orderByChild('date').startAt(startTime).on('value', snapshot => {
 
                 fechas = snapshot.val()
     
@@ -91,7 +89,10 @@ export default class ReservasClase extends Component {
                     firebase.database().ref(`/teachers/${key}/newCalendar/week/${llave}`).once('value', snapshot => {
 
                         if (snapshot.val().status != undefined && snapshot.val().status != null && snapshot.val().status == 1  ) {
-                            var date = moment(snapshot.val().date).locale('es').format('YYYY/MM/DD - LT')
+                    
+                            
+                            var temp = snapshot.val().date
+                            var date = moment(temp).format('DD-MM-YYYY HH:mm')
                             Dates.push(date)
                         }
     
@@ -99,6 +100,26 @@ export default class ReservasClase extends Component {
                    
                     
                 }
+                var fechasFinal = []
+                Dates.forEach(element => {
+                    var substr = element.split(" ")
+                    var index = fechasFinal.find(it => it == substr[0])
+                    if (index != undefined) {
+                        fechasFinal[index].push(substr[1])
+                
+                        
+                    } else {
+                        fechasFinal.push(substr[0])
+                        fechasFinal[substr[0]] = []
+                        fechasFinal[substr[0]].push(substr[1])
+                       
+                        
+                    }
+                });
+        
+                this.setState({EndTime:fechasFinal})
+
+                
                 this.setState({keycalendar:kc})
 
                 
@@ -110,16 +131,72 @@ export default class ReservasClase extends Component {
                 }
     
             })
-        }
-        else {
-            Toast.show('No se pueden reservar clases con fechas anteriores a la actual',5000)
+        
 
     }
+
+    componentDidMount  (){
+        this.setState({isLoading:true})
+        this.loadCalendar()
+
+        
     }
 
+    renderItem = ({item,index}) =>(
+        
+
+        <Card
+    
+        containerStyle={{
+          padding: 0, borderRadius: 20, shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 5,
+          },
+          shadowOpacity: 0.34,
+          shadowRadius: 6.27,
+    
+          elevation: 10,
+      
+        }}>
+    
+          <ListItem
+    
+            containerStyle={{
+              borderRadius: 20, shadowColor: "#000",
+              shadowOffset: {
+                width: 0,
+                height: 11,
+              },
+              shadowOpacity: 0.55,
+              shadowRadius: 14.78,
+    
+              elevation: 22,
+              height: 105,
+              elevation: 10,
+            }}
+        
+     
+          
+            title={item[index]}
+    
+            titleStyle={{ color: 'black', fontWeight: 'bold',fontSize:ancho('3.8%') }}
+            subtitle={item}
+            bottomDivider
+            chevron={
+                <TouchableOpacity onPress={()=>this.HandleReserva(item,skill,name,lastname,tipo,newSkill,key,keycalendar,time)} style={[styles.buttonContainer, styles.reservarButton]} >
+                <Text style={styles.reservastext}>RESERVAR</Text>
+                </TouchableOpacity>}
+
+  
+          />
+        </Card>
+    
+
+    )
 
     render() {
-        const {day,skill,name,lastname,tipo,newSkill,key,keycalendar,time} = this.state
+        const {skill,name,lastname,tipo,newSkill,key,keycalendar,time,calendar} = this.state
 
      
         
@@ -144,140 +221,34 @@ export default class ReservasClase extends Component {
 
                     />
                 </View>
-
-                {/* <View style={styles.centerContent}>
-                    <Calendar
-                        // the list of items that have to be displayed in agenda. If you want to render item as empty date
-                        // the value of date key kas to be an empty array []. If there exists no value for date key it is
-                        // considered that the date in question is not yet loaded
-                        // items={{
-                        //     '2019-10-22': [{ text: 'item 1 - any js object' }],
-                        //     '2019-10-23': [{ text: 'item 2 - any js object' }],
-                        //     '2019-10-24': [],
-                        //     '2019-10-25': [{ text: 'item 3 - any js object' }, { text: 'any js object' }]
-                        // }}
-                        // callback that gets called when items for a certain month should be loaded (month became visible)
-                        loadItemsForMonth={(month) => { console.log('trigger items loading') }}
-                        // callback that fires when the calendar is opened or closed
-                        onCalendarToggled={(calendarOpened) => { console.log(calendarOpened) }}
-                        // callback that gets called on day press
-                        onDayPress={(day) => { this.loadCalendar(day) }}
-                        // callback that gets called when day changes while scrolling agenda list
-                        onDayChange={(day) => { console.log('day changed') }}
-                        // initially selected day
-
-                        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-
-                        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-                        maxDate={'2050-05-30'}
-                        // Max amount of months allowed to scroll to the past. Default = 50
-                        pastScrollRange={50}
-                        // Max amount of months allowed to scroll to the future. Default = 50
-                        futureScrollRange={50}
-                        // specify how each item should be rendered in agenda
-                        // renderItem={(item, firstItemInDay) => {return (<View />);}}
-                        // specify how each date should be rendered. day can be undefined if the item is not first in that day.
-                        // renderDay={(day, item) => {return (<View />);}}
-                        // specify how empty date content with no items should be rendered
-                        // renderEmptyDate={() => {return (<View />);}}
-                        // specify how agenda knob should look like
-                        // renderKnob={() => {return (<View />);}}
-                        // specify what should be rendered instead of ActivityIndicator
-                        // renderEmptyData = {() => {return (<View />);}}
-                        // specify your item comparison function for increased performance
-                        rowHasChanged={(r1, r2) => { return r1.text !== r2.text }}
-                        // Hide knob button. Default = false
-                        hideKnob={false}
-                        // By default, agenda dates are marked if they have at least one item, but you can override this if needed
-                        markedDates={{
-                            '2012-05-16': { selected: true, marked: true },
-                            '2012-05-17': { marked: true },
-                            '2012-05-18': { disabled: true }
-                        }}
-                        // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
-                        onRefresh={() => console.log('refreshing...')}
-                        // Set this true while waiting for new data from a refresh
-                        refreshing={false}
-                        // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
-                        refreshControl={null}
-                        // agenda theme
-                        theme={{
-                            monthTextColor: '#00BEB1',
-                            selectedDayTextColor: '#00BEB1',
-                            textMonthFontWeight: 'bold',
-                            textMonthFontSize: ancho('4.5%'),
-                            textSectionTitleColor: '#00BEB1',
-                            selectedDayBackgroundColor: '#00BEB1',
-                            textDayFontWeight: '600',
-                            textDayFontSize: ancho('3.6%'),
-                            dayTextColor: '#9e9e9e',
-                            textDayHeaderFontSize: ancho('3.4%'),
-                            todayTextColor:'#00BEB1',
-                            arrowColor:'#00BEB1',
-                            textDisabledColor: '#d9e1e8',
-                            selectedDotColor: '#00BEB1',
-                            
-
-                            
-
-                    
-                        }}
-                       
-                        style={{
-                            marginTop: 20,
-                            borderBottomWidth: 1,
-                            borderBottomColor: "#9e9e9e", shadowOpacity: 12, shadowColor: "#000", shadowOffset: {
-                                width: 0,
-                                height: 6,
-                            },
-                            shadowOpacity: 0.37,
-                            shadowRadius: 7.49,
-                            elevation: 5,
-
-
-
-                        }}
-                    />
-
-                </View> */}
-                <View style={styles.bottoncontent}>
-                    
-
-                        {(() => {
-                            if (this.state.calendar.length >= 1 && this.state.calendar != undefined) {
-                                return (<View >
-                                    <View style={styles.containertext} >  
-                                        <Text style={styles.reservastext2} > { moment(day.dateString).locale('es').format('MMMM')} {moment(day.dateString).locale('es').format('D')}  </Text>
+                <View style={styles.containertext} >  
                                         <Text style={styles.textclases} > Clases disponibles </Text>   
-                                    </View>
-                                  
-                                    <ScrollView>
-                                    {this.state.calendar.map((item) => (
+                </View>
 
-                                        
-                                        <ListItem
-                                        style={{paddingLeft:20,paddingRight:20}}
-                                        containerStyle={{borderBottomWidth:1,borderBottomColor:"#9e9e9e",paddingLeft:5}}
-                                            title={item}
-                                            titleStyle={{color:"#757575"}}
-                                            chevron={
-                                            <TouchableOpacity onPress={()=>this.HandleReserva(item,skill,name,lastname,tipo,newSkill,key,keycalendar,time)} style={[styles.buttonContainer, styles.reservarButton]} >
-                                            <Text style={styles.reservastext}>RESERVAR</Text>
-                                            </TouchableOpacity>}
-                                            
-                                            bottomDivider
-                                            
 
-                                        />
-                                    
-                                    ))}
-                                    </ScrollView>
-                                    </View>
-                                )
-                            }
-                        })()}
+                <View style={styles.bottoncontent}>
+                
+                {
+            // this.state.isLoading
+            // ?<View style={{ paddingTop: 120, flex: 1, justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
+            //   <Image source={require('../../assets/spinner.gif')} style={{ height: alto('23%'), width: ancho('38%') }} />
+            //   <Text style={styles.textload}>Cargando profesores ....</Text>
+            // </View>
+             <FlatList
+              keyExtractor={(item, index) => 'key' + index}
+              initialNumToRender={15}
+              data={this.state.EndTime}
+              renderItem={this.renderItem}
+              get
+
+            />
+
+        }
+
+
+
                     
-
+                    
                 </View>
 
 
@@ -291,6 +262,23 @@ export default class ReservasClase extends Component {
 
 
 
+{/* <ListItem
+key={index}
+style={{paddingLeft:20,paddingRight:20}}
+title={item}
+titleStyle={{color:"#757575",alignContent:'center',alignItems:'center',justifyContent:'center'}}
+chevron={
+<TouchableOpacity onPress={()=>this.HandleReserva(item,skill,name,lastname,tipo,newSkill,key,keycalendar,time)} style={[styles.buttonContainer, styles.reservarButton]} >
+<Text style={styles.reservastext}>RESERVAR</Text>
+</TouchableOpacity>}
+
+bottomDivider
+
+/> */}
+
+
+
+
 const styles = StyleSheet.create({
     container: {
         width: '100%',
@@ -299,15 +287,15 @@ const styles = StyleSheet.create({
     top: {
         alignContent: 'center',
         width: '100%',
-        height: '5%',
-        paddingBottom: 60
+        height: '6%',
+     
     },  
-    centerContent: {
-        height: '55%',
-        width: '100%'
-    },
+    // centerContent: {
+    //     height: '55%',
+    //     width: '100%'
+    // },
     bottoncontent: {
-        height: "40%",
+        height: "95%",
         width: '100%',
         paddingBottom:30,
         paddingTop:10
@@ -357,5 +345,10 @@ const styles = StyleSheet.create({
         justifyContent:'center',
         paddingBottom:10
 
-    }
+    },    textload:{
+        paddingTop:15,
+        color:"#9E9E9E",
+        textShadowColor:'#75827d',
+        fontSize:ancho('4%')
+    },
 })
